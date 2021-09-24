76e0686c253d0d3176a3df91854b12330a3d84cb
Vladimir Prilepskiy <a89122285824@gmail.com>
2021-09-24 17:12:34 +0500

    add session transactionality for JMS

--- src/main/java/ru/skblub/config/JmsConfig.java
+++ src/main/java/ru/skblub/config/JmsConfig.java
@@ -3,9 +3,12 @@ package ru.skblub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
 
 @Configuration
 @EnableJms
@@ -13,10 +16,16 @@ public class JmsConfig {
 
     @Bean
     public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        var converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         return converter;
     }
 
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        var jmsTemplate = new JmsTemplate(connectionFactory);
+        jmsTemplate.setSessionTransacted(true);
+        return jmsTemplate;
+    }
 }
